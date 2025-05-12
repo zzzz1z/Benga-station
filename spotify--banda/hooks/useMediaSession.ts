fef09d9dc98554f82ff94e4091e2be6120633fca
zzz <you@example.com>
2025-05-12 09:56:04 +0100

    workplease2

--- spotify--banda/hooks/useMediaSession.ts
+++ spotify--banda/hooks/useMediaSession.ts
@@ -13,17 +13,18 @@ const useMediaSession = (
   useEffect(() => {
     if (!("mediaSession" in navigator)) return;
 
-    const artwork = song.image_path
+    const baseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const imagePath = song.image_path ? `storage/v1/object/public/imagens/${song.image_path}` : '';
+    const artwork = imagePath
       ? [
-          { src: song.image_path, sizes: "96x96", type: "image/jpeg" },
-          { src: song.image_path, sizes: "128x128", type: "image/jpeg" },
-          { src: song.image_path, sizes: "192x192", type: "image/jpeg" },
-          { src: song.image_path, sizes: "256x256", type: "image/jpeg" },
-          { src: song.image_path, sizes: "512x512", type: "image/jpeg" },
+          { src: `${baseUrl}/${imagePath}`, sizes: "96x96", type: "image/jpeg" },
+          { src: `${baseUrl}/${imagePath}`, sizes: "128x128", type: "image/jpeg" },
+          { src: `${baseUrl}/${imagePath}`, sizes: "192x192", type: "image/jpeg" },
+          { src: `${baseUrl}/${imagePath}`, sizes: "256x256", type: "image/jpeg" },
+          { src: `${baseUrl}/${imagePath}`, sizes: "512x512", type: "image/jpeg" }
         ]
       : [];
 
-    // Set media metadata for playback controls
     navigator.mediaSession.metadata = new MediaMetadata({
       title: song.title,
       artist: song.author,
@@ -31,7 +32,6 @@ const useMediaSession = (
       artwork,
     });
 
-    // Set media session action handlers
     navigator.mediaSession.setActionHandler("play", play);
     navigator.mediaSession.setActionHandler("pause", pause);
     navigator.mediaSession.setActionHandler("previoustrack", () => {
@@ -45,30 +45,12 @@ const useMediaSession = (
     });
     navigator.mediaSession.setActionHandler("nexttrack", player.playNext);
 
-    // Update the playback state to match the audio playback state
     navigator.mediaSession.playbackState = isPlaying ? "playing" : "paused";
 
-    // Optionally handle changes to the audio state directly in media session
-    const audio = document.querySelector("audio") as HTMLAudioElement | null;
-    if (audio) {
-      audio.onplay = () => {
-        if (!isPlaying) {
-          play();  // Synchronize the state if audio was played externally
-        }
-      };
-      audio.onpause = () => {
-        if (isPlaying) {
-          pause();  // Synchronize the state if audio was paused externally
-        }
-      };
-    }
-
-    // Cleanup media session metadata when component is unmounted or when the song changes
     return () => {
       navigator.mediaSession.metadata = null;
     };
   }, [isPlaying, song, play, pause, player]);
-
 };
 
 export default useMediaSession;
