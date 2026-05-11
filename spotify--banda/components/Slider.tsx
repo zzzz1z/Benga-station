c35b72455c3d6ea6995ad85aaa3198f9a0135d21
zzz3z <therealfakegiuliani@gmail.com>
2026-05-11 22:50:38 +0100

    1.3.63

--- spotify--banda/components/Slider.tsx
+++ spotify--banda/components/Slider.tsx
@@ -1,65 +1,32 @@
 'use client';
 
-import * as RadixSlider from '@radix-ui/react-slider'
-
-
+import * as RadixSlider from '@radix-ui/react-slider';
 
 interface SliderProps {
-    value?: number;
-    onChange?: (value: number) => void;
-};
-const Slider: React.FC<SliderProps> = ({
-    value = 1,
-    onChange
-
-}) => {
-
-    const handleChange = (newValue: number[]) => {
-        onChange?.(newValue[0])
-    }
-  return (
-    <RadixSlider.Root
-    className='
-     relative
-     flex
-     items-center
-     select-none
-     touch-none
-     w-full
-     h-10
-     '
-     defaultValue={[1]}
-     value={[value]}
-     onValueChange={handleChange}
-     max={1}
-     step={0.1}
-     aria-label='Volume'
-
-    >
-        <RadixSlider.Track
-         className='
-          bg-neutral-600
-          relative
-          grow
-          rounded-full
-          h-[3px]'
-        >
-            <RadixSlider.Range
-             className='
-              absolute
-              bg-white
-              rounded-full
-              h-full
-              '
-            />
-
-          
-
-        </RadixSlider.Track>
-
-    </RadixSlider.Root>
-    
-  )
+  value?: number;
+  onChange?: (value: number) => void;
 }
 
-export default Slider
+const Slider: React.FC<SliderProps> = ({ value = 1, onChange }) => {
+  const handleChange = (newValue: number[]) => {
+    onChange?.(newValue[0]);
+  };
+
+  return (
+    <RadixSlider.Root
+      className="relative flex items-center select-none touch-none w-full h-10"
+      value={[value]}
+      onValueChange={handleChange}
+      max={1}
+      step={0.01}
+      aria-label="Volume"
+    >
+      <RadixSlider.Track className="bg-neutral-600 relative grow rounded-full h-[3px]">
+        <RadixSlider.Range className="absolute bg-white rounded-full h-full" />
+      </RadixSlider.Track>
+      <RadixSlider.Thumb className="block w-3 h-3 bg-white rounded-full shadow focus:outline-none" />
+    </RadixSlider.Root>
+  );
+};
+
+export default Slider;
