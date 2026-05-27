6779844534123c48d3ede715bc411873aab260fc
zzz3z <therealfakegiuliani@gmail.com>
2026-05-27 01:42:24 +0100

    3.0.0.9

--- spotify--banda/capacitor.config.ts
+++ spotify--banda/capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.zzzz1z.bengastation',
   appName: 'Benga Station',
   webDir: 'out',
+  server: {
+    url: 'http://46.62.149.125',
+    cleartext: true,
+  },
 };
 
 export default config;
