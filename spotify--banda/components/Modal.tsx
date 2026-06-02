a2ee42211a10226097b521fffd3bb3e45417f044
zzz3z <therealfakegiuliani@gmail.com>
2026-06-02 11:49:32 +0100

    fix day 3 fix 2

--- spotify--banda/components/Modal.tsx
+++ spotify--banda/components/Modal.tsx
@@ -50,7 +50,7 @@ const Modal: React.FC<ModalProps> = ({
           focus:outline-none
           flex
           flex-col
-          overflow-hidden
+          overflow-scroll
         '>
           <Dialog.Title className='text-xl text-center font-bold mb-4'>
             {title}
